4c218f42cc3234135b615994510760ce5477c1b4
WiltonCarvalho <45881665+WiltonCarvalho@users.noreply.github.com>
2022-07-17 15:30:04 -0300

    Update install_docker_buildkit.sh

--- install_docker_buildkit.sh
+++ install_docker_buildkit.sh
@@ -6,7 +6,6 @@ BUILDX_VERSION=$(curl -fsL $BUILDX_RELESES | grep -m 1 -Eo 'v[0-9]+\.[0-9]+\.[0-
 curl -fsSL $BUILDX_RELESES/download/$BUILDX_VERSION/buildx-$BUILDX_VERSION.linux-amd64 \
   -o $HOME/.docker/cli-plugins/docker-buildx
 chmod +x $HOME/.docker/cli-plugins/docker-buildx
-BUILDKIT_RELESES="https://github.com/moby/buildkit/releases"
-BUILDKIT_VERSION=$(curl -fsL $BUILDKIT_RELESES | grep -m 1 -Eo 'v[0-9]+\.[0-9]+\.[0-9]*')
-docker buildx create --name img-builder --use --driver docker-container \
-  --driver-opt image=moby/buildkit:$BUILDKIT_VERSION --driver-opt network=host
+docker buildx create --name image-builder --use \
+  --driver docker-container \
+  --driver-opt network=host
