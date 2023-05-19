7079cbbe40e74d2289349fb6ddce8934b2ba37eb
WiltonCarvalho <45881665+WiltonCarvalho@users.noreply.github.com>
2023-05-19 17:06:07 -0300

    Update install_docker_buildkit.sh

--- install_docker_buildkit.sh
+++ install_docker_buildkit.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -ex
-mkdir -p $HOME/.docker/cli-plugins
-BUILDX_RELESES="https://github.com/docker/buildx/releases"
-BUILDX_VERSION=$(curl -fsL $BUILDX_RELESES | grep -m 1 -Eo 'v[0-9]+\.[0-9]+\.[0-9]*')
-curl -fsSL $BUILDX_RELESES/download/$BUILDX_VERSION/buildx-$BUILDX_VERSION.linux-amd64 \
-  -o $HOME/.docker/cli-plugins/docker-buildx
-chmod +x $HOME/.docker/cli-plugins/docker-buildx
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+
+COMPOSE_RELEASES="https://github.com/docker/compose/releases"
+COMPOSE_VERSION=$(curl -fsL $COMPOSE_RELEASES/latest | grep -m 1 -Eo 'v[0-9]+\.[0-9]+\.[0-9]*')
+COMPOSE_ARCH=$(uname -p)
+sudo curl -fSL# $COMPOSE_RELEASES/download/$COMPOSE_VERSION/docker-compose-linux-$COMPOSE_ARCH \
+  -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -sf /usr/local/bin/docker-compose /usr/local/lib/docker/cli-plugins/docker-compose
+docker-compose version
+
+BUILDX_RELEASES="https://github.com/docker/buildx/releases"
+BUILDX_VERSION=$(curl -fsL $BUILDX_RELEASES/latest | grep -m 1 -Eo 'v[0-9]+\.[0-9]+\.[0-9]*')
+if [ $(uname -p) == "aarch64" ]; then BUILDX_ARCH=arm64; else BUILDX_ARCH=amd64; fi
+sudo curl -fSL# $BUILDX_RELEASES/download/$BUILDX_VERSION/buildx-$BUILDX_VERSION.linux-$BUILDX_ARCH \
+  -o /usr/local/lib/docker/cli-plugins/docker-buildx
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx
+
+docker info | grep -A3 Plugins
 docker buildx create --name image-builder --use \
   --driver docker-container \
   --driver-opt network=host
